11582f5074f45ef137d979b3219e476181d4383d
Murilo Costa <murilo.nunes.jose@gmail.com>
2018-07-29 19:23:01 -0300

    Create CORS filter and closes #10

---
+++ src/main/java/br/com/yacatecuhtli/ApplicationConfiguration.java
@@ -0,0 +1,31 @@
+package br.com.yacatecuhtli;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class ApplicationConfiguration {
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedMethod(HttpMethod.GET);
+        corsConfiguration.addAllowedMethod(HttpMethod.POST);
+        corsConfiguration.addAllowedMethod(HttpMethod.PUT);
+        corsConfiguration.addAllowedMethod(HttpMethod.DELETE);
+        corsConfiguration.addAllowedHeader("Authorization");
+        corsConfiguration.addAllowedHeader("Content-Type");
+
+        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        corsConfigurationSource.registerCorsConfiguration("/api/**", corsConfiguration);
+        return new FilterRegistrationBean(new CorsFilter(corsConfigurationSource));
+    }
+
+}
+
